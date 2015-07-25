fa6a88336447f43caf0ebbc8c60d67430e1f8e70
sabinkhdk <mr.sabinkhadka@gmail.com>
2015-07-25 13:38:04 -0400

    plot 3

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # ExData_Plotting2
 Exploratory data analysis course project 2
 07/23/15:
-Files for plot 1: plot1.R
-Function used# aggregate(expressions, data, function)
+Files for plot 1: 
+plot1.R: Function used# aggregate(expressions, data, function)
 plots plot1.png: bar plot of total P2.5 Emissions each year
-Plot2: Similar plot of data from Baltimore city only.
+Plot2.R: Similar plot of data from Baltimore city only.
 Make subset of Data from Baltimore city fibs="24510"
+plot3.R: Total P2.5 emission per yr by type 
+    use qplot
---
+++ plot3.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+# load data
+NEI <- readRDS("./exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("./exdata-data-NEI_data/Source_Classification_Code.rds")
+# Subset data with data from Baltimore city
+DataBaltimore <- NEI[NEI$fips=="24510",]
+# Total by year by type 
+TotalEmissionsXyrXtype <- aggregate(Emissions ~ year + type, DataBaltimore, sum, simplify=TRUE)
+# Plot
+png("plot3.png")
+g1 <- qplot(x=year, y=Emissions, data=TotalEmissionsXyrXtype, geom="line", stat="identity", color = type)
+g1 <- g1 + ylab("Total P2.5 Emssions") + ggtitle("Total P2.5 Emissions in Baltimore city by type")
+plot(g1)
+dev.off()
